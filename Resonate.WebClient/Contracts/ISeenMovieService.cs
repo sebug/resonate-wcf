e2f72c069a723757700a7f2fe6f43417c63f3b3b
sebug <sebastian.gfeller@gmail.com>
2020-07-14 19:07:32 +0200

    Adding client-side service contract

---
+++ Resonate.WebClient/Contracts/ISeenMovieService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using Resonate.WebClient.Entities;
+
+namespace Resonate.WebClient.Contracts
+{
+    [ServiceContract]
+    public interface ISeenMovieService
+    {
+        [OperationContract]
+        SeenMovie Add(SeenMovie movie);
+
+        [OperationContract]
+        List<SeenMovie> GetAll();
+    }
+}
---
+++ Resonate.WebClient/Entities/SeenMovie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Resonate.WebClient.Entities
+{
+    [DataContract]
+    public class SeenMovie
+    {
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
