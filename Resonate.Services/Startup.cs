bc63a1cfb7ae32cffc0d434c34f0d12c862ebf44
sebug <sebastian.gfeller@gmail.com>
2020-07-14 09:36:38 +0200

    Adding seen movie service to startup

--- Resonate.Services/Startup.cs
+++ Resonate.Services/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreWCF;
+using CoreWCF.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Resonate.Services.Contracts;
 
 namespace Resonate.Services
 {
@@ -16,24 +19,17 @@ namespace Resonate.Services
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddServiceModelServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseServiceModel(builder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                builder.AddService<SeenMovieService>();
+                builder.AddServiceEndpoint<SeenMovieService, ISeenMovieService>(new BasicHttpBinding(),
+                    "SeenMovieService");
             });
         }
     }
