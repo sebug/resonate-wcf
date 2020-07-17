4980d19f9bc9a38dd15f371bfa1a974dfda8b84c
sebug <sebastian.gfeller@gmail.com>
2020-07-17 09:27:56 +0200

    View page basics

--- Resonate.WebClient/Controllers/SeenMovieController.cs
+++ Resonate.WebClient/Controllers/SeenMovieController.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Resonate.WebClient.Controllers
 {
     public class SeenMovieController : Controller
     {
+        private readonly ILogger<SeenMovieController> _logger;
 
+        public SeenMovieController(ILogger<SeenMovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
